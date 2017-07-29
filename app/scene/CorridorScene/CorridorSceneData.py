fd36f6a87df4f65a6817fe4810bbe60322608cd7
Nalia1991 <marie.44.56@hotmail.com>
2017-07-29 16:45:20 -0400

    Added Hud in other scene

--- app/scene/CorridorScene/CorridorSceneData.py
+++ app/scene/CorridorScene/CorridorSceneData.py
@@ -1,9 +1,8 @@
 import pygame
 
 from app.settings import *
+from app.sprites.LevelHUD import LevelHUD
 from app.sprites.PlayerPlateform import PlayerPlateform
-from ldLib.GUI.EnergyBar import EnergyBar
-from ldLib.GUI.LifeBar import LifeBar
 from ldLib.scene.SceneDataTMX import SceneDataTMX
 
 
@@ -26,10 +25,4 @@ class CorridorSceneData(SceneDataTMX):
         self.player = PlayerPlateform(playerInitx, playerInity, self)
         self.camera.add(self.player)
 
-        self.addHUD()
-
-    def addHUD(self):
-        playerLifeBar = LifeBar(self.player,PLAYER_LIFE_X,PLAYER_LIFE_Y,PLAYER_LIFE_WIDTH,PLAYER_LIFE_HEIGHT)
-        self.spritesHUD.add(playerLifeBar)
-        energyBar = EnergyBar(self.player, PLAYER_ENERGY_X, PLAYER_ENERGY_Y, PLAYER_ENERGY_WIDTH, PLAYER_ENERGY_HEIGHT)
-        self.spritesHUD.add(energyBar)
+        LevelHUD(self,self.player)
