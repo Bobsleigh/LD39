e914419671537195a7d8be0a5cc3e19fffe4dccd
Nalia1991 <marie.44.56@hotmail.com>
2017-07-29 15:35:58 -0400

    Player can be hurt

--- ldLib/tools/ImageBox.py
+++ ldLib/tools/ImageBox.py
@@ -14,3 +14,40 @@ class ImageBox:
             surface = pygame.Surface(dimension)
             surface.fill(color)
         return surface
+
+    def blackAndWhiteBorderBox(self, width, height, border):
+        surface = pygame.Surface((width,height),pygame.SRCALPHA)
+        surface.convert_alpha()
+        surface.blit(self.borderOnly(width,height,border),[0,0])
+
+        return surface
+
+    def borderOnly(self, width, height, border,color=BLACK):
+        width = int(width)
+        height = int(height)
+        border = int(border)
+        surface = pygame.Surface((width, height), pygame.SRCALPHA)
+
+        surface.convert_alpha()
+
+        # top line
+        topLine = pygame.Surface([width, border])
+        topLine.fill(WHITE)
+        surface.blit(topLine, [0, 0])
+
+        # bottom line
+        bottomLine = pygame.Surface([width, border])
+        bottomLine.fill(WHITE)
+        surface.blit(bottomLine, [0, height - border])
+
+        # left line
+        leftLine = pygame.Surface([border, height])
+        leftLine.fill(WHITE)
+        surface.blit(leftLine, [0, 0])
+
+        # right line
+        rightLine = pygame.Surface([border, height])
+        rightLine.fill(WHITE)
+        surface.blit(rightLine, [width - border, 0])
+
+        return surface
