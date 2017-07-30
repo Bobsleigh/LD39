5761571367377ffd884f6cc0dbe738b985ecb6f9
Nalia1991 <marie.44.56@hotmail.com>
2017-07-29 22:25:27 -0400

    Added a corridor example. (Bomb corridor level).

--- app/scene/CorridorScene/CorridorSceneData.py
+++ app/scene/CorridorScene/CorridorSceneData.py
@@ -3,11 +3,9 @@ import pygame
 from app.scene.LevelSceneData import LevelSceneData
 from app.settings import *
 
+
 class CorridorSceneData(LevelSceneData):
     def __init__(self):
         super().__init__("TestTmxData", "InZone_01")
-        self.sceneName = CORRIDOR_LEVEL_1
-        self.nextLevel = INSTRUCTION_SCENE
-
         self.sceneName = TEST_TMX_SCENE
         self.nextLevel = CORRIDOR_LEVEL_1
