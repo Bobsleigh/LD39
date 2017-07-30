0fb9058b039390460c17b0dcf367d2ad8f68e0d9
LAPTOP-PG7AKB81\comagnan <comagnan@coveo.com>
2017-07-29 20:16:27 -0400

    Maintenant les bullets sont bleus pis toute

--- app/scene/BombBossScene/BombBossSceneLogicHandler.py
+++ app/scene/BombBossScene/BombBossSceneLogicHandler.py
@@ -4,6 +4,7 @@ from ldLib.scene.LogicHandler import LogicHandler
 from app.scene.TopDownPhysics import TopDownPhysics
 from ldLib.collision.collisionNotifySprite import collisionNotifySprite
 from app.settings import *
+from app.sprites.Bullet import Bullet
 import pygame
 
 
@@ -16,6 +17,7 @@ class BombBossSceneLogicHandler(LogicHandler):
     def handle(self):
         super().handle()
         self.physics.update()
+        self.handleFriendlyBulletCollision()
         self.handleBulletCollision()
 
     def handleCollision(self):
@@ -25,11 +27,14 @@ class BombBossSceneLogicHandler(LogicHandler):
     def handleBulletCollision(self):
         collisionList = pygame.sprite.spritecollide(self.sceneData.player, self.sceneData.enemyProjectiles, False)
         for bullet in collisionList:
+            if isinstance(bullet, Bullet):
+                bullet.detonate()
             self.sceneData.player.hurt(10)
 
     def handleFriendlyBulletCollision(self):
         collisionList = pygame.sprite.spritecollide(self.sceneData.boss, self.sceneData.friendlyBullets, False)
         for bullet in collisionList:
+            bullet.detonate()
             self.sceneData.boss.hurt(10)
 
     def explodingBombCollision(self):
