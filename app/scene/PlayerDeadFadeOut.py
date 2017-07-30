c6522ddf2a85846ca7bcc65327f524c3e7de17c3
Nalia1991 <marie.44.56@hotmail.com>
2017-07-29 21:00:46 -0400

    Game over screen is more fleshed out.

---
+++ app/scene/PlayerDeadFadeOut.py
@@ -0,0 +1,21 @@
+import pygame
+from app.settings import *
+
+
+class PlayerDeadFadeOut():
+    def __init__(self, screen):
+        fontScreen = pygame.font.SysFont(FONT_NAME, 40)
+        message = fontScreen.render('You died! Get your revenge on those creeps!', True, LIFE_BAR_COLOR)
+        messagePos = [(SCREEN_WIDTH - message.get_width()) / 2,
+                      (SCREEN_HEIGHT - message.get_height()) / 2]
+
+        for i in range(100):
+            fadeInSurface = pygame.Surface([SCREEN_WIDTH, SCREEN_HEIGHT])
+            fadeInSurface.fill(BLACK)
+            fadeInSurface.set_alpha(i)
+            screen.blit(fadeInSurface, (0, 0))
+            if message != None:
+                screen.blit(message, messagePos)
+            pygame.time.wait(15)
+            pygame.display.flip()
+        pygame.time.wait(60)
--- app/sprites/LevelHUD.py
+++ app/sprites/LevelHUD.py
@@ -4,8 +4,12 @@ from ldLib.GUI.EnergyBar import EnergyBar
 from ldLib.GUI.LifeBar import LifeBar
 
 class LevelHUD:
-    def __init__(self,scenedata,player):
+    def __init__(self,scenedata,player,boss=None):
         playerLifeBar = LifeBar(player, PLAYER_LIFE_X, PLAYER_LIFE_Y, PLAYER_LIFE_WIDTH, PLAYER_LIFE_HEIGHT)
         scenedata.spritesHUD.add(playerLifeBar)
         energyBar = EnergyBar(player, PLAYER_ENERGY_X, PLAYER_ENERGY_Y, PLAYER_ENERGY_WIDTH, PLAYER_ENERGY_HEIGHT)
         scenedata.spritesHUD.add(energyBar)
+        if boss is not None:
+            #Add boss' life bar here.
+            pass
+
